0882e33e7155cbf5569bc56e7919c16d6687b316
Reful4ent <dima2323fr3@gmail.com>
2024-09-19 18:32:09 +0700

    finally all with product-calculate

--- src/api/product-calculate/controllers/product-calculate.ts
+++ src/api/product-calculate/controllers/product-calculate.ts
@@ -1,16 +1,29 @@
 import productCalculate from "../routes/product-calculate";
 
-//ToDo: сделать поиск по айди сущности а не через фронт))000
 export default{
   productCalculate: async(ctx) => {
+    let totalPrice = 0;
     console.log(ctx.request.body);
     const productCharacteristcs = ctx.request.body;
-    let price = productCharacteristcs.currentPrice.price
-      + productCharacteristcs.ingredient.price;
-    for (let i = 0; i < productCharacteristcs.addedIngredients.length; i++) {
-      price += productCharacteristcs.addedIngredients[i].ingredient.price * productCharacteristcs.addedIngredients[i].count;
+
+    const product = await strapi.entityService.findOne('api::product.product',productCharacteristcs.productId, {
+      fields: [],
+      populate: {
+        price: true
+      },
+    });
+
+
+    for(let i = 0; i < product.price.length; i++) {
+      if (product.price[i].name != productCharacteristcs.currentSize.name)
+        continue;
+      totalPrice += product.price[i].price;
     }
-    console.log(price);
-    return price;
+
+
+    const priceFromAdded = await strapi.service('api::product-calculate.product-calculate').getAddedIngredientsPrice(productCharacteristcs.addedIngredients);
+    totalPrice += priceFromAdded;
+
+    return totalPrice;
   }
 }
--- src/api/product-calculate/services/product-calculate.ts
+++ src/api/product-calculate/services/product-calculate.ts
@@ -1,3 +1,16 @@
 
+import { factories } from '@strapi/strapi';
 
-export default () => {}
+
+export default {
+  getAddedIngredientsPrice: async (addedIngredients) => {
+    let totalPrice = 0;
+    for (const ingredient of addedIngredients) {
+      const result = await strapi.entityService.findOne('api::ingredient.ingredient', ingredient.ingredient.id,{
+        fields: ['price']
+      })
+      totalPrice += result.price * ingredient.count;
+    }
+    return totalPrice;
+  }
+}
