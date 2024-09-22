19f4ec5472a749b432f1cd8a4ef8f4d603b16f18
h1km4t1ll0 <dolgovdaniil007@gmail.com>
2024-09-22 15:59:50 +0700

    add: докинул типы, дал по шапке за код

--- src/api/cart-calculate/controllers/cart-calculate.ts
+++ src/api/cart-calculate/controllers/cart-calculate.ts
@@ -1,31 +1,60 @@
-import cartCalculate from "../services/cart-calculate";
+'use strict';
 
-export default{
+import { Strapi } from '@strapi/strapi';
+
+export default ({ strapi }: { strapi: Strapi }) => ({
   cartCalculate: async (ctx) => {
     let totalPrice = 0;
     if (ctx.request.body.shopCart){
       const shopCart = ctx.request.body.shopCart;
+      // фор оправдан - внутри авейт все четко
       for (const cartItem of shopCart) {
 
         let tempPrice = 0;
-        const product = await strapi.entityService.findOne('api::product.product', cartItem.product.id, {
-          fields: [],
-          populate: {
-            price: true
-          },
-        });
+        const product = await strapi.entityService.findOne(
+          'api::product.product',
+          cartItem.product.id, {
+            fields: [],
+            populate: {
+              price: true
+            },
+          }
+        );
 
-        for(let i = 0; i < product.price.length; i++) {
-          if (product.price[i].id != cartItem.currentSize.id)
-            continue;
-          tempPrice += product.price[i].price;
-        }
+        // форы - нахуй
+        // for(let i = 0; i < product.price.length; i++) {
+        //   if (product.price[i].id != cartItem.currentSize.id)
+        //     continue;
+        //   tempPrice += product.price[i].price;
+        // }
 
-        for(const ingredient of cartItem.addedIngredients) {
-          const temp = await strapi.entityService.findOne('api::ingredient.ingredient', ingredient.ingredient.id, {
-            fields: ['price']
-          });
-          tempPrice += temp.price;
+        // редьюс - тема
+        let totalPrice = product.price.reduce((accumulator, element) => {
+          if (element.id == cartItem.currentSize.id) {
+            return accumulator + element.price;
+          }
+
+          return 0;
+        }, 0);
+
+
+        // тот случай, когда фор оправдан из-за авейта
+        // но фор говно, потому что нейминги говно
+        // for (const ingredient of cartItem.addedIngredients) {
+        const addedIngredientsId = cartItem.addedIngredients.map(
+          (addedIngredient) => addedIngredient.ingredient.id
+        );
+        for (const ingredientId of addedIngredientsId) {
+          // имена переменных:
+          // temp - говно
+          // ingredient - заебись
+          const ingredient = await strapi.entityService.findOne(
+            'api::ingredient.ingredient',
+            ingredientId, {
+              fields: ['price']
+            }
+          );
+          tempPrice += ingredient.price;
         }
 
         totalPrice += tempPrice * cartItem.count;
@@ -33,4 +62,4 @@ export default{
     }
     return totalPrice;
   }
-}
+});
--- src/api/product-calculate/controllers/product-calculate.ts
+++ src/api/product-calculate/controllers/product-calculate.ts
@@ -2,25 +2,43 @@ import productCalculate from "../routes/product-calculate";
 
 export default{
   productCalculate: async(ctx) => {
-    let totalPrice = 0;
-    const productCharacteristcs = ctx.request.body;
+    const productCharacteristics = ctx.request.body;
 
-    const product = await strapi.entityService.findOne('api::product.product',productCharacteristcs.productId, {
-      fields: [],
-      populate: {
-        price: true
+    const product = await strapi.entityService.findOne(
+      'api::product.product',
+      productCharacteristics.productId,
+      {
+        fields: [],
+        populate: {
+          price: true
+        },
       },
-    });
+    );
+
+    // не надо использовать форы без крайней необходимости - медленный кусок дерьма
+    // for(let i = 0; i < product.price.length; i++) {
+    //   if (product.price[i].id != productCharacteristics.currentSize.id)
+    //     continue;
+    //   totalPrice += product.price[i].price;
+    // }
+
+    // тип того должно быть:
+    let totalPrice = product.price.reduce((accumulator, element) => {
+        if (element.id == productCharacteristics.currentSize.id) {
+          return accumulator + element.price;
+        }
+
+        return 0;
+    }, 0);
 
 
-    for(let i = 0; i < product.price.length; i++) {
-      if (product.price[i].id != productCharacteristcs.currentSize.id)
-        continue;
-      totalPrice += product.price[i].price;
-    }
+    // следи за длиной строки, нихуя ж неудобно читать длинные строки))
+    const priceFromAdded = await strapi.service(
+      'api::product-calculate.product-calculate'
+    ).getAddedIngredientsPrice(
+      productCharacteristics.addedIngredients
+    );
 
-
-    const priceFromAdded = await strapi.service('api::product-calculate.product-calculate').getAddedIngredientsPrice(productCharacteristcs.addedIngredients);
     totalPrice += priceFromAdded;
 
     return totalPrice;
